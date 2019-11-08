2ade62414cb8c76dc225c4fe1b2a52b3a6267ae7
Grant Jackson <gej1515@gmail.com>
2019-11-07 19:15:19 -0600

    started backend of the file

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,67 @@
-var db = require("../models");
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
 
+// Dependencies
+// =============================================================
+var path = require("path");
+
+
+// Routes
+// =============================================================
 module.exports = function(app) {
-  // Load index page
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // add route loads the add.html page, where users can enter new books to the db
+  app.get("/browse", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/browse.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+  // all route loads the all.html page, where all books in the db are displayed
+  app.get("/portfolio", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/portfolio.html"));
   });
+
+  // short route loads the short.html page, where short books in the db are displayed
+  app.get("/search", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/search.html"));
+  });
+
+  
+
+
 };
+
+// var db = require("../models");
+
+// module.exports = function(app) {
+//   // Load index page
+//   app.get("/", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//         msg: "Welcome!",
+//         examples: dbExamples
+//       });
+//     });
+//   });
+
+//   // Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
