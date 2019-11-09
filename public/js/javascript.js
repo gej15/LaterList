5db97eb0084ca530e8c2e194199639f02014efa0
Grant Jackson <gej1515@gmail.com>
2019-11-09 10:01:32 -0600

    added firebase user to local storage

---
+++ public/js/javascript.js
@@ -0,0 +1,58 @@
+$(document).ready(function() {
+    // Execute some code here
+   
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyCMYpMAbjntYhCyxjGlGlbEezOYhxP6HQQ",
+      authDomain: "project2-b70da.firebaseapp.com",
+      databaseURL: "https://project2-b70da.firebaseio.com",
+      projectId: "project2-b70da",
+      storageBucket: "project2-b70da.appspot.com",
+      messagingSenderId: "494117517998",
+      appId: "1:494117517998:web:0da2a85b81a6a310134b3f",
+      measurementId: "G-SJ6H5B10ER"
+    };
+  
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    console.log("start")
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    var uiConfig = {
+        callbacks: {
+          signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            // Return type determines whether we continue the redirect automatically
+            // or whether we leave that to developer to handle.
+            var user = authResult.user;
+            localStorage.setItem('user', JSON.stringify(user))
+            console.log(user)
+           
+            return true;
+          },
+          uiShown: function() {
+            // The widget is rendered.
+            // Hide the loader.
+            document.getElementById('loader').style.display = 'none';
+          }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'default',
+        signInSuccessUrl: "/",
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+        
+        // Terms of service url.
+        tosUrl: '<your-tos-url>',
+        // Privacy policy url.
+        privacyPolicyUrl: '<your-privacy-policy-url>'
+      };
+
+      ui.start('#firebaseui-auth-container', uiConfig);
+
+      let user = JSON.parse(localStorage.getItem('firebaseui::rememberedAccounts'))
+
+      console.log(user)
+    
+})
