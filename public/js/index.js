761cff90156374a62ee2f7026962608872f4a8ee
Grant Jackson <gej1515@gmail.com>
2019-11-10 23:21:16 -0600

    added user id to  each page

--- public/js/index.js
+++ public/js/index.js
@@ -19,6 +19,45 @@ $(document).ready(function() {
 
   $('#signout').on('click', function(){
       localStorage.setItem('user', JSON.stringify(''))
-      location.href = '/portfolio'
+      location.href = '/search'
   })
+
+
+    let userId = ""
+    let email = ({
+      name: user.email
+    })
+
+    console.log(email)
+
+  function validateUser() {
+      $.get("/api/user", function(data){
+          console.log(data.length)
+          console.log(data)
+          let currentUserValid = false
+          if (data.length === 0) {
+              addUser()
+          } else {
+              for (let i = 0; i < data.length; i++){
+                  console.log(data)
+                  console.log(data.length)
+                  console.log(data[i].name)
+                  console.log(email.name)
+                  if (data[i].name === email.name ) {
+                      currentUserValid = true
+                      userId = data[i].id    
+                  } else {
+                      console.log('no')
+                  }
+              }
+              if (!currentUserValid) {
+                  console.log("new user")
+                  addUser()
+              } else {
+                  console.log('already a user')
+                  console.log(userId)  
+              }
+          }
+      })
+  }
 })
--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -15,11 +15,12 @@ $(document).ready(function() {
         console.log('there is a user')
         $('#welcome').text('Welcome ' + user.displayName )
         $('#login').css('display', 'none')
+        validateUser()
     }
 
     $('#signout').on('click', function(){
         localStorage.setItem('user', JSON.stringify(''))
-        location.href = '/portfolio'
+        location.href = '/search'
     })
 
     let email = ({
@@ -27,58 +28,56 @@ $(document).ready(function() {
     })
     let userId = ""
     
-
-    $.get("/api/user", function(data){
-        console.log(data.length)
-        console.log(data)
-        let currentUserValid = false
-        if (data.length === 0) {
-            addUser()
-        } else {
-            for (let i = 0; i < data.length; i++){
-                console.log(data)
-                console.log(data.length)
-                console.log(data[i].name)
-                console.log(email.name)
-                if (data[i].name === email.name ) {
-                    currentUserValid = true
-                    userId = data[i].id    
-                } else {
-                    console.log('no')
+    function validateUser() {
+        $.get("/api/user", function(data){
+            console.log(data.length)
+            console.log(data)
+            let currentUserValid = false
+            if (data.length === 0) {
+                addUser()
+            } else {
+                for (let i = 0; i < data.length; i++){
+                    console.log(data)
+                    console.log(data.length)
+                    console.log(data[i].name)
+                    console.log(email.name)
+                    if (data[i].name === email.name ) {
+                        currentUserValid = true
+                        userId = data[i].id    
+                    } else {
+                        console.log('no')
+                    }
                 }
-            }
                 if (!currentUserValid) {
                     console.log("new user")
                     addUser()
                 } else {
                     console.log('already a user')
-                    console.log(userId)
-
-                    let item = ({
-                        catagory: "podcast",
-                        title:  "akward turtles",
-                        itemId: "j256asig23rf",
-                        UserId: userId,
-                    })
-                
-                
-                    function addAnItem(){
-                        $.post("/api/newItem", item)
-                        .then(function(item){
-                            console.log(item)
-                        })
-                    }
-                    addAnItem()
+                    console.log(userId)  
                 }
-            
             }
-
-        function addUser() {
-            $.post("/api/new", email)
-            .then(function(email) {
-                // Log the data we found
-            console.log(email);
-            })
-        }
+        })
+    }
+    let item = ({
+        catagory: "podcast",
+        title:  "akward turtles",
+        itemId: "j256asig23rf",
+        UserId: userId,
     })
+
+    function addAnItem(){
+        $.post("/api/newItem", item)
+        .then(function(item){
+            console.log(item)
+        })
+    }
+    // addAnItem()
+
+    function addUser() {
+        $.post("/api/new", email)
+        .then(function(email) {
+            // Log the data we found
+        console.log(email);
+        })
+    }
 })  
