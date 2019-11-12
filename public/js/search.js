b95a61b46da706fea520155f305c202210e242dc
Grant Jackson <gej1515@gmail.com>
2019-11-11 19:37:55 -0600

    added podcast ajax call to search.js

--- public/js/search.js
+++ public/js/search.js
@@ -27,6 +27,29 @@ $(document).ready(function() {
     let email = ({
         name: user.email
     })
+
+ 
+    function callPodcast() {
+        const podQueryURL = 'https://itunes.apple.com/search?term=' + search +'&entity=podcast&limit=5'
+        $.ajax({
+            url:  podQueryURL,
+            method: "GET",
+            // The name of the callback parameter
+            jsonp: "callback",
+
+            // Tell jQuery we're expecting JSONP
+            dataType: "jsonp",
+            // Work with the response
+            success: function( response ) {
+            console.log( response ); // server response
+            }
+        })
+    }
+    
+
+    callPodcast()
+    
+    
    
     
     function validateUser() {
@@ -60,49 +83,50 @@ $(document).ready(function() {
         })
     }
 
-    let movie = $(this).val("#movie-input");
-        // let movie = $('#movie-form').val();
-        const queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=c21b2973";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-            // console.log(response)
-            $("#search-movie").on("click", function(event){
-            console.log(movie)
-            event.preventDefault();
-            console.log("I work!")
-            console.log(response.Title)
-            const movieDiv = $("<div class='movie'>");
-            //   ------------------------------------------
-            const id = response.imdbID;
-            const pID = $("<p>").text("ID: " + id);
-            movieDiv.append(pID);
-            //   -----------------------------------------------
-            const rating = response.Rated;
-            const pOne = $("<p>").text("Rating: " + rating);
-            movieDiv.append(pOne);
-            //   ---------------------------------------------
-            const released = response.Released;
-            const pTwo = $("<p>").text("Released: " + released);
-            movieDiv.append(pTwo);
-            //   -----------------------------------------------
-            const plot = response.Plot;
-            const pThree = $("<p>").text("Plot: " + plot);
-            movieDiv.append(pThree);
-            //   ------------------------------------------------
-            const review = response.imdbRating;
-            const pFour = $("<p>").text("Score: " + review);
-            movieDiv.append(pFour);
-            //   ------------------------------------------------
-            const genre = response.Genre;
-            const pFive = $("<p>").text("Genre: " + genre);
-            movieDiv.append(pFive);
-            //   ------------------------------------------------
-            const imgURL = response.Poster;
-            const image = $("<img>").attr("src", imgURL);
-            movieDiv.append(image);
-            $("#movies-view").prepend(movieDiv);
-            });
-    })
+
+//     let movie = $(this).val("#movie-input");
+//         // let movie = $('#movie-form').val();
+//         const queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=c21b2973";
+//         $.ajax({
+//             url: queryURL,
+//             method: "GET"
+//         }).then(function(response) {
+//             // console.log(response)
+//             $("#search-movie").on("click", function(event){
+//             console.log(movie)
+//             event.preventDefault();
+//             console.log("I work!")
+//             console.log(response.Title)
+//             const movieDiv = $("<div class='movie'>");
+//             //   ------------------------------------------
+//             const id = response.imdbID;
+//             const pID = $("<p>").text("ID: " + id);
+//             movieDiv.append(pID);
+//             //   -----------------------------------------------
+//             const rating = response.Rated;
+//             const pOne = $("<p>").text("Rating: " + rating);
+//             movieDiv.append(pOne);
+//             //   ---------------------------------------------
+//             const released = response.Released;
+//             const pTwo = $("<p>").text("Released: " + released);
+//             movieDiv.append(pTwo);
+//             //   -----------------------------------------------
+//             const plot = response.Plot;
+//             const pThree = $("<p>").text("Plot: " + plot);
+//             movieDiv.append(pThree);
+//             //   ------------------------------------------------
+//             const review = response.imdbRating;
+//             const pFour = $("<p>").text("Score: " + review);
+//             movieDiv.append(pFour);
+//             //   ------------------------------------------------
+//             const genre = response.Genre;
+//             const pFive = $("<p>").text("Genre: " + genre);
+//             movieDiv.append(pFive);
+//             //   ------------------------------------------------
+//             const imgURL = response.Poster;
+//             const image = $("<img>").attr("src", imgURL);
+//             movieDiv.append(image);
+//             $("#movies-view").prepend(movieDiv);
+//             });
+
 })
